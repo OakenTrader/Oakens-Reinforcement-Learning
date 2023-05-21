896ebaf82097e5e3bb06c5cfbff900c1e675918d
OakenTrader <49554534+OakenTrader@users.noreply.github.com>
2023-05-21 09:40:01 +0000

    21 April

--- games/Salesman.py
+++ games/Salesman.py
@@ -30,12 +30,20 @@ class Salesman(Game):
         player["score"] = 0
         player["values"] = np.array([1 for i in range(self.n_points)])
         player["pos"] = 0
+        player["values"][0] = 0
         player["history"] = [self.poses[0]]
 
+    def distance(self, pos1, pos2):
+        return np.linalg.norm(pos1 - pos2)
+
+    def score(dist, value):
+        return value - dist
+        
+    
     def traverse(self, player, pos):
-        dist = np.sum((self.poses[pos] - self.poses[player["pos"]]) ** 2) ** 0.5
+        dist = self.distance(self.poses[pos], self.poses[player["pos"]])
         player["pos"] = pos
-        player["score"] -= dist + 1
+        player["score"] += self.score(dist, player["values"][pos])
         player["values"][pos] = 0
         player["history"].append(self.poses[pos])
 
@@ -71,3 +79,59 @@ class Salesman(Game):
 
 
             print(f"Total Score: {player['score']}")
+
+class Salesman_RL(Salesman):
+    def __init__(self):
+        super().__init__()
+        self.states = None
+        self.encoder = dict()
+
+    def get_action_space(self):
+        return np.arange(0,self.n_points)
+    
+    def create_table(self):
+        n = self.n_points
+        return np.zeros(((2 ** n) * n, n))
+
+    def state_encoding(self, state, action):
+        def encode(pos, mark):
+            ncp = 0
+            for i,m in enumerate(mark):
+                ncp += 2 ** i * m
+            ncp += (pos) * 2 ** (len(mark))
+            self.encoder[mark][pos] = ncp
+        mark, pos = state
+        mark = tuple(mark)
+        encoder = self.encoder
+        if mark in encoder.keys():
+            if pos not in encoder[mark]:
+                encode(pos, mark)
+        else:
+            self.encoder[mark] = dict()
+            encode(pos, mark)
+            
+        return encoder[mark][pos], action
+
+
+    def transition(self, state, action):
+        mark, pos = state
+        mark_new = mark.copy()
+        mark_new[action] = 0
+        new_pos = action
+        new_state, _ = self.state_encoding(mark_new, new_pos, action)
+        trans_prob = dict()
+        trans_prob[new_state] = {"prob": 1, "new_pos":action, "new_mark":mark_new}
+        return trans_prob
+
+    def reward(self, state, action):
+        mark, pos = state
+        trans_prob = self.transition(mark, pos, action)
+        reward = 0
+        for state in trans_prob:
+            new_pos = state["new_pos"]
+            new_mark = state["new_mark"]
+            dist = self.distance(pos, new_pos)
+            score = np.sum(np.abs(mark - new_mark))
+            reward += state["prob"] * (score - dist)
+        return reward
+        
